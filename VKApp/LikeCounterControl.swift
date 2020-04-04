3cd044aaac26c90207b633c90996ab25afd4a4b4
Vladislav Likhachev <mymindissharp@gmail.com>
2020-04-04 21:29:15 +0300

    добавил счетчик лайков

--- VKApp/LikeCounterControl.swift
+++ VKApp/LikeCounterControl.swift
@@ -9,35 +9,50 @@
 import UIKit
 
 class LikeCounterControl: UIControl {
-    
+    var countOfLikes : UInt = 0
+    var like : Bool = false
     var counterLabel = UILabel()
     var iconButton = UIButton()
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        addSubview(iconButton)
-        NSLayoutConstraint.activate([
-            iconButton.topAnchor.constraint(equalTo: topAnchor),
-            iconButton.bottomAnchor.constraint(equalTo: bottomAnchor),
-            iconButton.trailingAnchor.constraint(equalTo: trailingAnchor)
-        ])
-    }
+
     
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        backgroundColor = .clear
+    override func awakeFromNib() {
+        super.awakeFromNib()
         setupView()
-        addSubview(iconButton)
-        NSLayoutConstraint.activate([
-            iconButton.topAnchor.constraint(equalTo: topAnchor),
-            iconButton.bottomAnchor.constraint(equalTo: bottomAnchor),
-            iconButton.trailingAnchor.constraint(equalTo: trailingAnchor)
-        ])
     }
     
     private func setupView() {
-        iconButton.setTitle("♡", for: .normal)
-        iconButton.setTitleColor(.lightGray, for: .normal)
-        iconButton.setTitleColor(.white, for: .selected)
+        backgroundColor = .clear
+        iconButton.tintColor = like ? .red : .white
+        iconButton.setImage(UIImage(named: "heart"), for: .normal)
+        iconButton.addTarget(self, action: #selector(changeCounter(_:)), for: .touchUpInside)
+        
+        counterLabel.text = String(countOfLikes)
+        counterLabel.font = .boldSystemFont(ofSize: CGFloat(9))
+        
+        let stack = UIStackView()
+        stack.addArrangedSubview(counterLabel)
+        stack.addArrangedSubview(iconButton)
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        addSubview(stack)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stack.topAnchor.constraint(equalTo: topAnchor),
+            stack.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stack.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stack.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
     }
     
+    @objc private func changeCounter(_ sender: UIButton) {
+        if like {
+            countOfLikes-=1
+            sender.tintColor = .white
+        } else {
+            countOfLikes+=1
+            sender.tintColor = .red
+        }
+        like = !like
+        counterLabel.text = String(countOfLikes)
+    }
 }
