d47a61f7f1283f1145335d3184fd894637ea5a80
Vladislav Likhachev <mymindissharp@gmail.com>
2020-04-05 12:17:57 +0300

    Переделал tableviewcontroller

--- VKApp/FriendsController.swift
+++ VKApp/FriendsController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class FriendsController: UITableViewController {
+class FriendsController: UIViewController {
     var sorterView = SorterBarControl()
     
+    @IBOutlet weak var tableView: UITableView!
     var users : [User] = [
         User(name: "Владислав", surname: "Лихачев", avatar: "vladislav"),
         User(name: "Евгений", surname: "Ёлчев", avatar: "eugene"),
@@ -33,18 +34,7 @@ class FriendsController: UITableViewController {
         ])
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int { 1 }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { users.count }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as! FriendTableViewCell
-        let user = users[indexPath.row]
-        cell.userLabel.text = "\(user.name) \(user.surname)"
-        cell.userLabel.font = .systemFont(ofSize: CGFloat(16))
-        cell.photoView.imageView.image = user.avatar
-        return cell
-    }
+
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "photoAlbumSegue" {
@@ -59,4 +49,18 @@ class FriendsController: UITableViewController {
     
 }
 
+extension FriendsController : UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int { 1 }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { users.count }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as! FriendTableViewCell
+        let user = users[indexPath.row]
+        cell.userLabel.text = "\(user.name) \(user.surname)"
+        cell.userLabel.font = .systemFont(ofSize: CGFloat(16))
+        cell.photoView.imageView.image = user.avatar
+        return cell
+    }
+}
 
