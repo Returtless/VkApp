a9b72a08c51b086f811944bd24cb8047bbb693d3
Vladislav Likhachev <mymindissharp@gmail.com>
2020-04-08 23:07:32 +0300

    Поправил навбар

--- VKApp/NavigationTabBar.swift
+++ VKApp/NavigationTabBar.swift
@@ -16,16 +16,25 @@ class NavigationTabBar: UITabBar {
     }
     func setTabBarItems(){
         
+        self.tintColor = .white
+        self.unselectedItemTintColor = .darkGray
+        
         let myTabBarItem1 = (items?[0])! as UITabBarItem
-        myTabBarItem1.image = UIImage(named: "friendsIcon")?.withRenderingMode(UIImage.RenderingMode.automatic)
+        myTabBarItem1.image = UIImage(systemName: "person.fill")?.withRenderingMode(UIImage.RenderingMode.automatic)
         myTabBarItem1.image?.withTintColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
-        myTabBarItem1.title = nil
+        myTabBarItem1.title = "Друзья"
         myTabBarItem1.badgeColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         
         
+        
         let myTabBarItem2 = (items?[1])! as UITabBarItem
-        myTabBarItem2.image = UIImage(named: "groupsIcon")?.withRenderingMode(UIImage.RenderingMode.automatic)
+        myTabBarItem2.image = UIImage(systemName: "person.3.fill")?.withRenderingMode(UIImage.RenderingMode.automatic)
         myTabBarItem2.image?.withTintColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
-        myTabBarItem2.title = nil
+        myTabBarItem2.title = "Группы"
+        
+        let myTabBarItem3 = (items?[2])! as UITabBarItem
+               myTabBarItem3.image = UIImage(systemName: "house.fill")?.withRenderingMode(UIImage.RenderingMode.automatic)
+               myTabBarItem3.image?.withTintColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+               myTabBarItem3.title = "Новости"
     }
 }
