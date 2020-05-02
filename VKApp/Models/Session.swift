c215f3cadf2d525dde5d240eefacd6bba1478a5b
Vladislav Likhachev <mymindissharp@gmail.com>
2020-05-02 11:57:32 +0300

    Добавил класс сессии

---
+++ VKApp/Models/Session.swift
@@ -0,0 +1,16 @@
+//
+//  Session.swift
+//  VKApp
+//
+//  Created by Владислав Лихачев on 02.05.2020.
+//  Copyright © 2020 Vladislav Likhachev. All rights reserved.
+//
+
+import Foundation
+
+class Session {
+    var token : String = ""
+    var iserId : Int = 0
+    
+    static let instance = Session()
+}
